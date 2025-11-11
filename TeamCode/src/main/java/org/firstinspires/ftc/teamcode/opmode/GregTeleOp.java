a6845d0c2f349abe8a349b50496f43e2c9fce75d
ecefour15 <95937284+ecefour15@users.noreply.github.com>
2025-11-11 15:18:33 -0500

    i love you lebron

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/GregTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/GregTeleOp.java
@@ -1,13 +1,16 @@
 package org.firstinspires.ftc.teamcode.opmode;
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.subsystem.Drive;
 
 import dev.nextftc.core.components.SubsystemComponent;
 import dev.nextftc.ftc.NextFTCOpMode;
 
+@TeleOp
 public class GregTeleOp extends NextFTCOpMode {
     private Drive drive;
-
     @Override
     public void onInit() {
         drive = new Drive(hardwareMap);
@@ -24,5 +27,8 @@ public class GregTeleOp extends NextFTCOpMode {
                 gamepad1.left_stick_y,
                 gamepad1.right_stick_x
         );
+
+        telemetry.addData("gamepad1 left_stick_x:", gamepad1.left_stick_x);
+        telemetry.update();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Drive.java
@@ -6,9 +6,12 @@ import com.pedropathing.ftc.drivetrains.Mecanum;
 import com.pedropathing.ftc.drivetrains.MecanumConstants;
 import com.pedropathing.ftc.localization.constants.PinpointConstants;
 import com.pedropathing.ftc.localization.localizers.PinpointLocalizer;
+import com.pedropathing.geometry.Pose;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.pedropathing.Constants;
 
 import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.subsystems.Subsystem;
@@ -18,17 +21,9 @@ public class Drive implements Subsystem {
     private final Follower follower;
 
     public Drive(HardwareMap hwm) {
-        this.follower = new Follower(
-                new FollowerConstants(),
-                new PinpointLocalizer(
-                        hwm,
-                        new PinpointConstants()
-                ),
-                new Mecanum(
-                        hwm,
-                        buildConstants()
-                )
-        );
+        this.follower = Constants.createFollower(hwm);
+        follower.startTeleopDrive();
+        follower.update();
     }
 
     private MecanumConstants buildConstants() {
@@ -38,7 +33,13 @@ public class Drive implements Subsystem {
         return mecanumConstants;
     }
 
+    @Override
+    public void initialize() {
+        follower.startTeleopDrive();
+    }
+
     public void drive(double x, double y, double r) {
+        follower.update();
         follower.setTeleOpDrive(y, x, r, true);
     }
 }
